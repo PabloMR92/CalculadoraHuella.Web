751caa3ece1e08dbbb7ee8f32ef54425fafc5c97
PabloMR92 <pablomr92@hotmail.com>
2017-08-24 14:47:44 -0300

    MockUp

---
+++ CalculadoraHuella.Mockup/startbootstrap-scrolling-nav-gh-pages/js/scrolling-nav.js
@@ -0,0 +1,47 @@
+var sticky = document.querySelector('.sticky');
+
+var container = document.querySelector('#container');
+
+$('.sticky').scrollTop(120);
+
+var origOffsetY = $('.sticky').scrollTop();
+
+
+
+$(document).ready(function() {
+
+    $("#container").scroll(function() {
+
+        if ($(this).scrollTop() > origOffsetY) {
+
+            sticky.classList.add('fixed');
+
+            $('.sticky').css({
+
+                top: $(this).scrollTop() + 'px'
+
+            });;
+
+            $('.sticky-pusher').css({
+
+                'height': $('.sticky').outerHeight() + 'px'
+
+            });
+
+        } else {
+
+            $('.sticky-pusher').css({
+
+                'height': '0px'
+
+            });
+
+            sticky.classList.remove('fixed');
+
+        }
+
+        console.log($('.sticky').scrollTop());
+
+    });
+
+});
