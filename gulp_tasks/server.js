bdd2ae4fea76a53091d694fdeb326dce524067c5
PabloMR92 <pablomr92@hotmail.com>
2017-09-30 20:40:44 -0300

    no message

--- gulp_tasks/server.js
+++ gulp_tasks/server.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var webserver = require('gulp-webserver');
 
 gulp.task('server', function() {
-    return gulp.src('./src/').pipe(webserver({
+    return gulp.src('./dist/').pipe(webserver({
         port: process.env.PORT || 5000,
         https: false,
         open: true
