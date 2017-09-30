8f58c4d54569c258f36f5a5577a16810d56d435b
PabloMR92 <pablomr92@hotmail.com>
2017-09-30 20:28:54 -0300

    test heroku

--- gulp_tasks/server.js
+++ gulp_tasks/server.js
@@ -2,8 +2,9 @@ var gulp = require('gulp');
 var webserver = require('gulp-webserver');
 
 gulp.task('server', function() {
-    return gulp.src('./dist/').pipe(webserver({
+    return gulp.src('./src/').pipe(webserver({
         port: process.env.PORT || 5000,
+        host: process.env.HOST || "0.0.0.0",
         https: false,
         open: true
     }));
