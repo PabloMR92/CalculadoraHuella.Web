0b68366316f931f91ab17f70f7c7451d3053f4a7
PabloMR92 <pablomr92@hotmail.com>
2017-09-30 20:34:34 -0300

    cambio host

--- gulp_tasks/server.js
+++ gulp_tasks/server.js
@@ -4,7 +4,6 @@ var webserver = require('gulp-webserver');
 gulp.task('server', function() {
     return gulp.src('./src/').pipe(webserver({
         port: process.env.PORT || 5000,
-        host: process.env.HOST || "0.0.0.0",
         https: false,
         open: true
     }));
