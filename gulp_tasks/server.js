76741054d493007ed7b5b3dda9291d5e55908268
PabloMR92 <pablomr92@hotmail.com>
2017-09-30 21:13:25 -0300

    host

--- gulp_tasks/server.js
+++ gulp_tasks/server.js
@@ -3,8 +3,9 @@ var webserver = require('gulp-webserver');
 
 gulp.task('server', function() {
     return gulp.src('./dist/').pipe(webserver({
+        host: '0.0.0.0',
         port: process.env.PORT || 5000,
         https: false,
         open: true
     }));
-})
+});
